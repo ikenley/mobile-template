69b70287aed162eb1051e71b8db4317f555edca7
ikenley <ikenley6@gmail.com>
2023-07-30 13:56:55 -0400

    createNativeStackNavigator (#3)

--- App.js
+++ App.js
@@ -1,5 +1,26 @@
-import Foo from "./src/Foo";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import DetailsScreen from "./src/DetailsScreen";
+import HomeScreen from "./src/HomeScreen";
+import { RootStackParamList } from "./src/types";
+
+const Stack = createNativeStackNavigator(RootStackParamList);
 
 export default function App() {
-  return <Foo />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Overview" }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          initialParams={{ itemId: 2001 }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "./types";
 import Api from "./Api";
 
-export const Foo = () => {
+type Props = NativeStackScreenProps<RootStackParamList, "Home">;
+
+export const Foo = ({ navigation }: Props) => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Open the pod bay doors, HAL.</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          navigation.navigate("Details", { itemId: 12345 });
+        }}
+      />
       <Api />
       <StatusBar style="auto" />
     </View>
